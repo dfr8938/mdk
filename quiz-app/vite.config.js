96ab727cc9088542b72f497b3cb6341ea92ac81b
Denis Frolov <d.fr8938@gmail.com>
2026-03-07 19:57:52 +0300

    first commit

---
+++ quiz-app/vite.config.js
@@ -0,0 +1,8 @@
+// Автор: Denis Frolov <d.fr8938@gmail.com/>
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
